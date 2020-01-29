8891857d37c7709d283b93f44dd9e7516cd826f8
Fran <hola@beachlab.org>
2020-01-29 07:31:30 +0100

    tweak editor window

--- js/mods.js
+++ js/mods.js
@@ -1479,7 +1479,7 @@
       var text = document.createElement('textarea')
       text.setAttribute('id', 'edit_module_text')
       text.style.width = '100%'
-      text.style.height = '100%'
+      text.style.height = '95%'
       text.value = def
       win.document.body.appendChild(text)
       function reload_module(idnumber) {
